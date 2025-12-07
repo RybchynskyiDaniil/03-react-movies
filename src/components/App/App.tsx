cc002c6fd069a6a4a1addb583d5239bb4ea6c125
Daniil Rybchynskyi <rybchynskyidaniil@gmail.com>
2025-12-07 19:57:15 +0000

    d

---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+// import { useState } from 'react'
+import fetchMovies from '../../services/moviesService';
+
+export default function App() {
+  
+
+}
+
+
---
+++ src/services/moviesService.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import type { Movie } from "../types/movies";
+
+interface MovieHTTPResponse {
+  results: Movie[];
+}
+export default async function fetchMovies (query: string): Promise<Movie[]> {
+    const myKey = import.meta.env.VITE_TMDB_TOKEN;
+    const response = await axios.get<MovieHTTPResponse>('https://api.themoviedb.org/3/search/movie', {
+        params: {
+            query: `${query}`,
+        },
+        headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${myKey}`,
+        }
+    })
+    return  response.data.results
+}
+
